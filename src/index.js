bfad155d820234519e8e5b26d63a2fc7fe440468
Shivam <shivam.singh@hikecommerce.com>
2026-07-06 14:14:05 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
-import express from "express";
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
-const app = express();
+
 
 connectDB()
 .then(() => {
