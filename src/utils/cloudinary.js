8619e78240bdb3b06b01e3358ac3a41dc719a131
Shivam <shivam.singh@hikecommerce.com>
2026-07-06 12:26:08 +0530

    added the upload file code using cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        const response = await cloudinary.v2.uploader.upload(localFilePath, { resource_type: "auto" });
+
+        console.log("File uploaded to Cloudinary successfully.", response.url);
+
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // Delete the locally saved file if upload fails
+        console.error("Error uploading file to Cloudinary.", error);
+        return null;
+    }
+}
+
+export { uploadOnCloudinary };
