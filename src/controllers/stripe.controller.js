e79b3260cb26f5ccbfd78cf6bc541c5b21201f42
Shivam <shivam.singh@hikecommerce.com>
2026-07-24 18:49:45 +0530

    attachPaymentMethod controller logic added

--- src/controllers/stripe.controller.js
+++ src/controllers/stripe.controller.js
@@ -3,74 +3,125 @@ import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { User } from "../models/user.model";
-import { createCustomerOnStripe, createSetupIntentOnStripe } from "../utils/stripe";
+import {
+  createCustomerOnStripe,
+  createSetupIntentOnStripe,
+  attachPaymentMethodOnStripe,
+  setDefaultPaymentMethodOnStripe,
+} from "../utils/stripe";
 
 const createStripeCustomer = asyncHandler(async (req, res) => {
-    const userId = req.user._id;
+  const userId = req.user._id;
 
-    const user = await User.findById(userId);
-    if(!user) {
-        throw new ApiError(404, "user not found");
-    }
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "user not found");
+  }
 
-    if(user.stripeCustomerId) {
-        return res.status(200).json(
-            new ApiResponse(
-                200,
-                {
-                    stripeCustomerId: user.stripeCustomerId
-                },
-                "Stripe customer already exists"
-            )
-        );
-    }
+  if (user.stripeCustomerId) {
+    return res.status(200).json(
+      new ApiResponse(
+        200,
+        {
+          stripeCustomerId: user.stripeCustomerId,
+        },
+        "Stripe customer already exists",
+      ),
+    );
+  }
 
-    const stripeCustomer = await createCustomerOnStripe({
-        name: user.fullName,
-        email: user.email
-    });
+  const stripeCustomer = await createCustomerOnStripe({
+    name: user.fullName,
+    email: user.email,
+  });
 
-    user.stripeCustomerId = stripeCustomer.id;
+  user.stripeCustomerId = stripeCustomer.id;
 
-    await user.save({
-        validateBeforesave: false
-    });
+  await user.save({
+    validateBeforesave: false,
+  });
 
-    return res.status(201).json(
-        new ApiResponse(
-            201,
-            user,
-            "stripeCustomer created successfully"
-        )
-    )
+  return res
+    .status(201)
+    .json(new ApiResponse(201, user, "stripeCustomer created successfully"));
 });
-
 
 // this controller Generate a Setup Intent and then Return client_secret to frontend
 const createSetupIntent = asyncHandler(async (req, res) => {
-    const userId = req.user._id;
+  const userId = req.user._id;
 
-    const user = await User.findById(userId);
-    if(!user){
-        throw new ApiError(404, "User not found")
-    }
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
 
-    if(!user.stripeCustomerId) {
-        throw new ApiError(404, "Stripe customer doesn't exist")
-    }
+  if (!user.stripeCustomerId) {
+    throw new ApiError(404, "Stripe customer doesn't exist");
+  }
 
-    const setupIntent = await createSetupIntentOnStripe(user.stripeCustomerId);
+  const setupIntent = await createSetupIntentOnStripe(user.stripeCustomerId);
 
-    return res.status(201).json(
-        new ApiResponse(
-            201,
-            {
-                clientSecret: setupIntent.client_secret
-            },
-            "Setup intent created"
-        )
-    )
-})
+  return res.status(201).json(
+    new ApiResponse(
+      201,
+      {
+        clientSecret: setupIntent.client_secret,
+      },
+      "Setup intent created",
+    ),
+  );
+});
 
 
-export { createStripeCustomer }
+const attachPaymentMethod = asyncHandler(
+    async (req, res) => {
+  const { paymentMethodId } = req.body;
+
+  if (!paymentMethodId?.trim()) {
+    throw new ApiError(400, "paymentMethod id is not required");
+  }
+
+  const userId = req.user._id;
+
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const stripeCustomerId = user.stripeCustomerId;
+  if (!stripeCustomerId) {
+    throw new ApiError(404, "stripeCustomer Id doesn't exist");
+  }
+
+  const attachedPaymentMethod = await attachPaymentMethodOnStripe(
+    paymentMethodId,
+    stripeCustomerId,
+  );
+
+  const defaultPaymentMethod = await setDefaultPaymentMethodOnStripe(
+    paymentMethodId,
+    stripeCustomerId,
+  );
+
+  const savedPaymentMethod = await PaymentMethod.create({
+    owner: user._id,
+    stripePaymentMethodId: attachedPaymentMethod.is,
+    brand: attachedPaymentMethod.card.brand,
+    last4: attachedPaymentMethod.card.last4,
+    expiryMonth: attachedPaymentMethod.card.exp_month,
+    expiryYear: attachedPaymentMethod.card.exp_year,
+    isDefault: true,
+  });
+
+  user.defaultPaymentMethodId = paymentMethodId;
+
+  await user.save({
+    validateBeforeSave: false
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, savedPaymentMethod, "paymentMethodId is updated successfully"));
+});
+
+export { createStripeCustomer, createSetupIntent, attachPaymentMethod };
--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -1,4 +1,5 @@
 import Stripe from "stripe";
+import { asyncHandler } from "./asyncHandler";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -22,4 +23,25 @@ const createSetupIntentOnStripe = async (stripeCustomerId) => {
   return setupIntent;
 };
 
-export { createCustomerOnStripe, createSetupIntentOnStripe };
+const attachPaymentMethodOnStripe = async (paymentMethodId, stripeCustomerId) => {
+  return await stripe.paymentMethods.attach(paymentMethodId, {
+    customer: stripeCustomerId,
+  });
+};
+
+const setDefaultPaymentMethodOnStripe = async(stripeCustomerId, paymentMethodId) => {
+  return await stripe.customers.update(stripeCustomerId, {
+    invoice_settings: {
+      default_payment_method: paymentMethodId,
+    },
+  });
+}
+
+
+const recordUsage = asyncHandler( async(req, res) => {
+  
+})
+
+export { createCustomerOnStripe, createSetupIntentOnStripe, attachPaymentMethodOnStripe,
+  setDefaultPaymentMethodOnStripe
+ };
